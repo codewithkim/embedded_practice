ff2fa129af6b1e9493bffc08337baabac7dd48a0
root <root@DESKTOP-316CLGJ.localdomain>
2026-02-23 16:37:32 -0700

    chore: integrate FreeRTOS kernel and configure CMake build system
    - Add FreeRTOS-Kernel as third_party library
    - Configure FreeRTOSConfig.h for POSIX simulator
    - Setup CMakeLists.txt to link pthread and rt libraries

---
+++ include/FreeRTOSConfig.h
@@ -0,0 +1,32 @@
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+/* Mandatory Setting: Simulation env */
+#define configUSE_PREEMPTION                    1
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configCPU_CLOCK_HZ                      ( ( unsigned long ) 1000000 )
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES                    ( 5 )
+#define configMINIMAL_STACK_SIZE                ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) ( 64 * 1024 ) )
+#define configMAX_TASK_NAME_LEN                 ( 16 )
+#define configUSE_16_BIT_TICKS                  0
+#define configIDLE_SHOULD_YIELD                 1
+
+/* API setting */
+#define INCLUDE_vTaskDelay                      1
+#define INCLUDE_vTaskPrioritySet                1
+#define INCLUDE_vTaskDelete                     1
+#define INCLUDE_vTaskCleanUpResources           0
+#define INCLUDE_vTaskSuspend                    1
+#define INCLUDE_vTaskDelayUntil                 1
+
+/* POSIX Simulator */
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
+#define configUSE_TIMERS                        1
+#define configTIMER_TASK_PRIORITY               ( configMAX_PRIORITIES - 1 )
+#define configTIMER_QUEUE_LENGTH                5
+#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
+
+#endif /* FREERTOS_CONFIG_H */
